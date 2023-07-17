3efa6eec4442b42a1f240d8686f7bf5494a2e933
Amaury Franssen <amauryfranssen@gmail.com>
2023-07-17 16:12:06 +0200

    feat(fonts): add Noto fonts with a direct link and raw files in fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import TestApi from './component/testApi'
-import './styles/main.scss'
+import './styles/style.scss'
 
 function App() {
   const [count, setCount] = useState(0)
