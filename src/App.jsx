f6dd2903730c5151db26040ccd390f821c69ac25
Amaury Franssen <amauryfranssen@gmail.com>
2023-07-17 15:53:05 +0200

    feat(styles): add styles for some useful pages as 404 and file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import TestApi from './component/testApi'
+import './styles/main.scss'
 
 function App() {
   const [count, setCount] = useState(0)
