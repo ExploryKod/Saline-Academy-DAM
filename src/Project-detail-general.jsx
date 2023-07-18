5da0b63211d9978b03d68bba3ed39d766f1837dd
Bifried <97131086+Bifried@users.noreply.github.com>
2023-07-19 01:12:33 +0200

    Detail projet & Detail projet modifications part 1

---
+++ src/Project-detail-general.jsx
@@ -0,0 +1,194 @@
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import './Project-detail-general.css'
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PauseIcon from '@mui/icons-material/Pause';
+import CancelIcon from '@mui/icons-material/Cancel';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
+
+function ProjectDetailGeneral() {
+    
+    const [description, setDescription] = React.useState('');
+    const handleChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const steps = [
+    'Programmation',
+    'Captation',
+    'Post-production',
+    'Editorial',
+    'Publication',
+  ];
+
+  const [openPriority, setPriorityOpen] = React.useState(false);
+  const [openPause, setPauseOpen] = React.useState(false);
+  const [openDelete, setDeleteOpen] = React.useState(false);
+
+  const priorityDialogOpen = () => {
+    setPriorityOpen(true);
+  };
+
+  const pauseDialogOpen = () => {
+    setPauseOpen(true);
+  };
+
+  const deleteDialogOpen = () => {
+    setDeleteOpen(true);
+  };
+
+  const handleClose = () => {
+    setPriorityOpen(false);
+    setPauseOpen(false);
+    setDeleteOpen(false);
+  };
+
+  return (
+    <>
+    <div className='project-detail-background'>
+        <h1 className='project-detail-title'>Enregistrement Cello</h1>
+        <section className='project-detail-section'>
+            <div className='tab-menu'>
+                tab-menu
+            </div>
+            <div className='edit-button'>
+                <Link to="/project-detail-general-update" className='editButton'><EditIcon className='editIcon'></EditIcon></Link>
+            </div>
+            <div className='project-detail-section1-content'>
+                <span>Professeur : Denis Severin</span>
+                <br></br>
+                <span className='project-detail-instrument'>Instrument : Cello</span>
+                <div className='project-detail-description-section'>
+                     <EmojiEventsIcon className='trophyIcon'></EmojiEventsIcon>
+                     <Accordion>
+                         <AccordionSummary
+                         expandIcon={<ExpandMoreIcon />}
+                          aria-controls="project-detail-description-content"
+                            id="project-detail-description-header">
+                            <Typography>Description</Typography>
+                         </AccordionSummary>
+                         <AccordionDetails>
+                            <Typography>
+                            Professor of cello at the Geneva University of Music and the Bern University of Arts.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+                <div className='division'> </div>
+                <section>
+                    <div className='project-detail-section2-content'>
+                        <h2 className='project-detail-title2'>Statut du projet</h2>
+                        <div className='project-detail-stepper'>
+                        <Box sx={{ width: '200%' }}>
+                            <Stepper activeStep={0} alternativeLabel>
+                                {steps.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step> ))}
+                            </Stepper>
+                        </Box>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+        <div className='project-detail-footer'>
+        <Stack spacing={2} direction="row">
+            <Button variant="contained" color='secondary' onClick={priorityDialogOpen} startIcon={<PriorityHighIcon />}>Prioriser</Button>
+            <Dialog
+        open={openPriority}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title-priority">
+          {"Prioriser le projet"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-priority">
+          Appuyer sur le bouton valider mettra le projet en priorité dans les projet en cours.<br></br><br></br> Êtes vous sûr de vouloir continuer ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color='secondary' startIcon={<CancelIcon />} onClick={handleClose}>Annuler</Button>
+          <Button variant="contained" color='secondary' startIcon={<CheckCircleIcon />} onClick={handleClose} autoFocus>
+            Valider
+          </Button>
+        </DialogActions>
+      </Dialog>
+            <Button variant="contained" color='secondary' onClick={pauseDialogOpen} startIcon={<PauseIcon />}>Mettre en pause</Button>
+            <Dialog
+        open={openPause}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title-pause">
+          {"Mettre en pause le projet"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-pause">
+          Appuyer sur le bouton valider mettra en pause toute avancée du projet pour une durée indéterminée. <br></br><br></br> Êtes vous sûr de vouloir continuer ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color='secondary'startIcon={<CancelIcon />} onClick={handleClose}>Annuler</Button>
+          <Button variant="contained" color='secondary' startIcon={<CheckCircleIcon />} onClick={handleClose} autoFocus>
+            Valider
+          </Button>
+        </DialogActions>
+      </Dialog>
+            <Button variant="contained" color='error' onClick={deleteDialogOpen} startIcon={<DeleteIcon />}>Supprimer</Button>
+            <Dialog
+        open={openDelete}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title-delete"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Supprimer le projet"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-delete">
+          Appuyer sur le bouton supprimer supprimera définitivement le projet. Ceci empêchera toutes modifications par la suite.<br></br><br></br> Êtes vous sûr de vouloir continuer ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color='secondary' startIcon={<CancelIcon />} onClick={handleClose}>Annuler</Button>
+          <Button variant="contained" color='secondary' startIcon={<DeleteIcon />} onClick={() => navigate("/")} autoFocus>
+            Supprimer
+          </Button>
+        </DialogActions>
+      </Dialog>
+        </Stack>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ProjectDetailGeneral
---
+++ src/Projet-detail-general-update.jsx
@@ -0,0 +1,175 @@
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import './Project-detail-general-update.css'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/Save';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CancelIcon from '@mui/icons-material/Cancel';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useNavigate } from "react-router-dom";
+
+
+function ProjectDetailGeneralUpdate() {
+
+    const navigate = useNavigate();
+    const [description, setDescription] = React.useState('');
+    const [professor, setProfessor] = React.useState('');
+
+    const handleChange = (event) => {
+      setDescription(event.target.value);
+      setProfessor(event.target.value);
+    };
+  
+    const [openCancel, setCancelOpen] = React.useState(false);
+    const [openSave, setSaveOpen] = React.useState(false);
+
+  
+    const cancelDialogOpen = () => {
+      setCancelOpen(true);
+    };
+  
+    const saveDialogOpen = () => {
+      setSaveOpen(true);
+    };
+  
+    const handleClose = () => {
+      setCancelOpen(false);
+      setSaveOpen(false);
+    };
+  
+    return (
+      <>
+      <div className='project-detail-background'>
+          <h1 className='project-detail-title'>Enregistrement Cello</h1>
+          <section className='project-detail-section'>
+              <div className='tab-menu'>
+                  tab-menu
+              </div>
+              <div className='project-detail-section1-content'>
+              <h2 className='project-detail-update-title'>Modifier les informations</h2>
+                <div className='select-flex'>
+                <span className='select-span'>Professeur :</span>
+                    <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Professeur</InputLabel>
+                        <Select
+                        labelId="professor-select-label"
+                        id="professor-select"
+                        value={professor}
+                        label="Professeur"
+                        onChange={handleChange}
+                        >
+                        <MenuItem value={'Denis Severin'}>Denis Severin</MenuItem>
+                        <MenuItem value={'Mathilda Catcher'}>Mathilda Catcher</MenuItem>
+                        <MenuItem value={'Louis Leland'}>Louis Leland</MenuItem>
+                        </Select>
+                    </FormControl>
+                    </Box>
+                </div>
+                  <br></br>
+                  <div className='select-flex'>
+                    <span className='project-detail-instrument select-span'>Instrument :</span>
+                  <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="instrument-select-label">Instrument</InputLabel>
+                        <Select
+                        labelId="instrument-select-label"
+                        id="instrument-select"
+                        value={professor}
+                        label="Instrument>"
+                        onChange={handleChange}
+                        >
+                        <MenuItem value={'Denis Severin'}>Cello</MenuItem>
+                        <MenuItem value={'Mathilda Catcher'}>Violon</MenuItem>
+                        <MenuItem value={'Louis Leland'}>Piano</MenuItem>
+                        </Select>
+                    </FormControl>
+                    </Box>
+                  </div>
+
+                  <div className='project-detail-description-section'>
+                       <EmojiEventsIcon className='trophyIcon'></EmojiEventsIcon>
+                       <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="description-select-label">Description</InputLabel>
+                        <Select
+                        labelId="description-select-label"
+                        id="description-select"
+                        value={professor}
+                        label="Description"
+                        onChange={handleChange}
+                        >
+                        <MenuItem value={'Denis Severin'}>Denis Severin est un musicien de grande envergure qui a fait beaucoup de blah blah blah blah blah blah blah</MenuItem>
+                        <MenuItem value={'Mathilda Catcher'}>Mathilda Catcher est une très belle violoniste d'origine portugaise qui blah blah blah blah blah blah</MenuItem>
+                        <MenuItem value={'Louis Leland'}>Louis Leland est un pianiste agée mais toujours dans la recherche de nouveauté blah blah blah blah blah</MenuItem>
+                        </Select>
+                    </FormControl>
+                    </Box>
+                  </div>
+              </div>
+          </section>
+          <div className='project-detail-footer'>
+          <Stack spacing={2} direction="row">
+              <Button variant="contained" color='secondary' onClick={cancelDialogOpen} startIcon={<CancelIcon />}>Annuler</Button>
+              <Dialog
+          open={openCancel}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title-cancel"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Annuler les modifications"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description-cancel">
+            Appuyer sur le bouton annuler annulera toutes vos modifications. <br></br><br></br> Êtes vous sûr de vouloir continuer ?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" color='secondary' startIcon={<CancelIcon />} onClick={handleClose}>Annuler</Button>
+            <Button variant="contained" color='secondary' startIcon={<CheckCircleIcon />} onClick={() => navigate("/project-detail-general")} autoFocus>
+              Valider
+            </Button>
+          </DialogActions>
+        </Dialog>
+              <Button variant="contained" color='error' onClick={saveDialogOpen} startIcon={<SaveIcon />}>Enregistrer</Button>
+              <Dialog
+          open={openSave}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title-update">
+            {"Enregistrer les modifications"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description-update">
+            Appuyer sur le bouton valider enregistrera toutes vos modifications et engendrera la perte des informations précédentes. <br></br><br></br> Êtes vous sûr de vouloir enregistrer vos modifications ?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" color='secondary'startIcon={<CancelIcon />} onClick={handleClose}>Annuler</Button>
+            <Button variant="contained" color='secondary' startIcon={<CheckCircleIcon />} onClick={handleClose} autoFocus>
+              Valider
+            </Button>
+          </DialogActions>
+        </Dialog>
+          </Stack>
+          </div>
+      </div>
+      </>
+    )
+  }
+
+export default ProjectDetailGeneralUpdate
