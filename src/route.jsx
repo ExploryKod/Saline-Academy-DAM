115204d1978a6f7ce1294a7e3251c18f79e60996
Amaury Franssen <amauryfranssen@gmail.com>
2023-07-19 16:57:13 +0200

    refactor: trying to be the closer as I can of master before rebase

--- src/route.jsx
+++ src/route.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, useContext } from "react";
-import Connexion from "./routes/connexion"
+import { Navigate, Route, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AppProvider } from './AppContext';
 import { AppContext } from './AppContext';
+import App from "./App";
+import Connexion from "./routes/connexion"
 import NotFoundPage from './routes/404';
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import App from "./App"
@@ -10,29 +13,34 @@ import ProjectDetailGeneral from "./Project-detail-general";
 import ProjectDetailGeneralUpdate from "./Projet-detail-general-update";
 
 const RoutesComponent = () => {
-    const isUser = false;
     const { sessionData, setSessionData } = useContext(AppContext);
 
     useEffect(() => {
         setSessionData({ session_id: sessionStorage.getItem('idToken'), session_email: sessionStorage.getItem('emailToken') });
-      }, []);
+    }, []);
 
-      console.log('session_id',sessionData.session_id);
+    console.log('session_id', sessionData.session_id);
 
-  return (
-      <>
-      {sessionData.session_id ? (
-          <Routes>
-              <Route path="/" element={<Navigate to="/homepage" replace />} />
-              <Route path="/homepage" element={<App />} />
-              <Route path='*' element={<NotFoundPage />} />
-          </Routes>):(
-              <Routes>
-                  <Route path="/" element={<Connexion />} />
-                  <Route path='*' element={<NotFoundPage />} />
-              </Routes>)}
-      </>
-  );
+    return (
+        <Router>
+            <AppProvider>
+                {sessionData.session_id ? (
+                <Routes>
+                    <Route path="/" element={<Navigate to="/homepage" replace />} />
+                    <Route path="/homepage" element={<App />} />
+                    <Route path="/test" element={<div>Not found</div>} />
+                    <Route path="/create" element={<CreateProject />} />
+                    <Route path="/project-detail-general" element={<ProjectDetailGeneral/>} />
+                    <Route path="/project-detail-general-update" element={<ProjectDetailGeneralUpdate/>} />
+                    <Route path="/projet-detail-add-rush" element={<AddRushPage/>}/>
+                </Routes>):(
+                    <Routes>
+                        <Route path="/" element={<Connexion />} />
+                        <Route path='*' element={<NotFoundPage />} />
+                    </Routes>)}
+                </AppProvider>
+        </Router>
+    );
 };
 
 export default RoutesComponent;
